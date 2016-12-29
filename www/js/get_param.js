ce1539cf87b00e3118c040b9f40014470a352db0
ken4310 <k-shimizu@sensiv.jp>
2016-12-29 20:44:27 +0900

    フォーム送信をjsで制御

---
+++ www/js/get_param.js
@@ -0,0 +1,24 @@
+  function GetQueryString() {
+    var result = {};
+    if (1 > window.location.search.length) {
+      return result;
+    } else {
+      // 最初の1文字 (?記号) を除いた文字列を取得する
+      var query = window.location.search.substring(1);
+      // クエリの区切り記号 (&) で文字列を配列に分割する
+      var parameters = query.split('&');
+      for (var i = 0; i < parameters.length; i++) {
+        // パラメータ名とパラメータ値に分割する
+        var element = parameters[i].split('=');
+        var paramName = decodeURIComponent(element[0]);
+        var paramValue = decodeURIComponent(element[1]);
+        // パラメータ名をキーとして連想配列に追加する
+        result[paramName] = paramValue;
+      }
+    }
+    return result;
+  }
+var params = GetQueryString();
+
+window.alert(params.category);
+
