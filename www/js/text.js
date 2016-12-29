4c37cb9258e674c9b5a925d27501c3b06f08896a
ken4310 <k-shimizu@sensiv.jp>
2016-12-29 16:15:22 +0900

    lanch.json追加

--- www/js/text.js
+++ www/js/text.js
@@ -1,14 +1,32 @@
 // ポップアップウィンドウに()内の内容出力する
-window.alert("hello");
+// window.alert("hello");
 // [window]は省略可能。『""』『’’』どちらで囲んでもよい
-alert('good by');
+// alert('good by');
 // 状況に応じて『””』と『’’』を使い分けよう
-alert("<a href='index.html'>");
+// alert("<a href='index.html'>");
 
 // HTMLページ内に出力する
-document.write("have a nice day!");
+// document.write("have a nice day!");
 
 //HTMLタグも出力できる
-document.write("<h1>have a nice day!</h1>");
+//document.write("<h1>have a nice day!</h1>");
 
 // ブラウザ、エディタのデバッグ画面、またはコマンドプロンプト画面に出力する
+console.log("My name is");
+//すべてconsole.logで出力しよう。
+
+// 変数の定義
+var hensu;
+hensu =100;
+// コマンドラインに出力。変数にダブルクォーテーションは必要ない・
+console.log(hensu);
+
+hensu = 500;
+console.log(hensu);
+
+//変数は計算,結合できる
+
+var vNum1 =100;
+var vNum2 =200;
+var kotae = vNum1 + vNum2;
+console.log(kotae);
--- www/text.md
+++ www/text.md
@@ -6,3 +6,12 @@
 変数の定義
 Javaの場合
 javascript(以下JS)の場合
+
+デバッグの方法
+window.alert();
+ブラウザF12開発者ツール→Console→Watch
+Visual Studio Code
+デバッグ(虫アイコン)→設定(歯車アイコン)
+下記部分を変更
+"program": "${workspaceRoot}『jsファイルのフルパス』
+デバッグボタン(右矢印アイコン)クリックでスタート
