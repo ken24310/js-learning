c00939047306eb30856c016143453f2b1d25cec9
ken4310 <k-shimizu@sensiv.jp>
2017-01-09 07:39:53 +0900

    lesson4完成

---
+++ www/lesson6/lesson6_1.js
@@ -0,0 +1,31 @@
+jQuery(function ($) {
+  //ここをクリックボタンクリックでリストが開く
+  $(".slide_list dt").click(function () {
+    $(".slide_list dd").slideToggle("fast");
+  });
+
+  //リスト表示ボタンとボックス表示ボタンで切り替え
+  $("#change_list").click(function () {
+    $("#img_grp").attr("class", "img_list");
+  });
+
+  $("#change_box").click(function () {
+    $("#img_grp").attr("class", "img_box");
+  });
+
+
+  //スクロールしたとき「ページトップへ」ボタン表示
+  $(window).scroll(function () {
+    $(".goto_page_top").show("slow");
+  });
+
+  //セレクトボックスチェンジで画像を変更
+  $(".select_img select").change(function () {
+    var img_num; //セレクトボックスの番号を格納
+    var file_name; //画像ファイル名を格納
+    img_num = $(".select_img select").val();
+    file_name = "images/" + img_num + ".jpg";
+    $(".select_img img").attr("src",file_name);
+  });
+
+});
